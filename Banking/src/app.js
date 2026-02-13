41f2630134ee3d06cf50791a34593c1ed0b5e094
reyanshrai <reyanshrai05@gmail.com>
2026-02-13 23:18:12 +0530

    remove user model file

--- Banking/src/app.js
+++ Banking/src/app.js
@@ -1,6 +1,22 @@
 import express from "express"
+import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth.routes.js"
+import accountRoutes from "./routes/account.routes.js"
+import transactionRoutes from "./routes/transaction.routes.js"
+
+
 
 const app = express()
 
+app.use(express.json())
+app.use(cookieParser())
+
+/** 
+ * Use Routes
+ */
+
+app.use("/api/auth",authRoutes)
+app.use("/api/accounts",accountRoutes)
+app.use("/api/transactions",transactionRoutes)
 
 export default app
